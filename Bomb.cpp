8c0d714acc1f7481de7a91e4f0c5e201aab2257f
auto-format <auto-format@auto.format>
2021-12-17 04:17:48 +0000

    Autoformat

--- Bomb.cpp
+++ Bomb.cpp
@@ -1,14 +1,14 @@
+#include <fstream>
 #include <iostream>
+#include <stdlib.h>
 #include <string.h>
-#include<fstream>
-#include<stdlib.h>
 
 using namespace std;
 
-int main()
-{   ofstream fout;
-    fout.open("file.txt");
-	for(int i=10000000;i>=0;i--){
-		fout<<"Hello";
-	}
+int main() {
+  ofstream fout;
+  fout.open("file.txt");
+  for (int i = 10000000; i >= 0; i--) {
+    fout << "Hello";
+  }
 }
